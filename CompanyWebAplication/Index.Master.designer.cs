17a10858fb48488590b312a8d64a8a38c6a62227
Mayank Sharma <minkimac@live.com>
2018-04-24 22:40:05 +0530

    Login form

--- CompanyWebAplication/Index.Master.designer.cs
+++ CompanyWebAplication/Index.Master.designer.cs
@@ -20,5 +20,14 @@ namespace CompanyWebAplication {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder Carousel1;
+        
+        /// <summary>
+        /// SubmitBtn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button SubmitBtn;
     }
 }
