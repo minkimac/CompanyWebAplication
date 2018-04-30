f5c423f755f1022a1ec6a0453e102f12344dcf66
Shivam Kumar <shivam.kumar@sahaj.biz>
2018-04-30 12:55:21 +0530

    Changes for auto text box searching.

---
+++ CompanyWebAplication/DataHandler.ashx.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Script.Serialization;
+
+namespace CompanyWebAplication
+{
+    /// <summary>
+    /// Summary description for DataHandler
+    /// </summary>
+    public class DataHandler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string term = context.Request["term"] ?? "";
+
+            List<string> Listdata = new List<string>();
+
+            string cs = ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+
+                SqlCommand cmd = new SqlCommand("sp_ArtifactsAutoCompletionforAjaxSearch", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.Add(new SqlParameter
+                {
+                    ParameterName = "@inputString",
+                    Value = term
+                });
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Listdata.Add(rdr["ArtifactName"].ToString());
+                   
+                }
+
+                
+            }
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            context.Response.Write(js.Serialize(Listdata));
+
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
